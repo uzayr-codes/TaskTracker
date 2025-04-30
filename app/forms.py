7be572e29a776efc8371ec3e71218ac3fb795b9b
uzayr-codes <pirachauzair7@gmail.com>
2025-05-01 03:58:16 +0500

    Your commit message

--- app/forms.py
+++ app/forms.py
@@ -2,7 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField, DateField, SelectField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
-class RegisterForm(FlaskForm):
+class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
